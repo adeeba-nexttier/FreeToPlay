d56716279b94b042e7113e4c9206d436998430eb
Adeeba Khan <adeeba.khan@nexttier.io>
2023-06-26 15:29:14 +0500

    set-up packages and retrofit

---
+++ app/src/main/java/com/dev/freetoplay/data/remote/dto/GameDto.kt
@@ -0,0 +1,39 @@
+package com.dev.freetoplay.data.remote.dto
+
+import com.dev.freetoplay.domain.model.Game
+import com.google.gson.annotations.SerializedName
+
+data class GameDto(
+    val developer: String,
+    /* Converting to Camel Notation */
+    @SerializedName("freetogame_profile_url")
+    val freeToGameProfileUrl: String,
+    @SerializedName("game_url")
+    val gameUrl: String,
+    val genre: String,
+    val id: Int,
+    val platform: String,
+    val publisher: String,
+    @SerializedName("release_date")
+    val releaseDate: String,
+    @SerializedName("short_description")
+    val shortDescription: String,
+    val thumbnail: String,
+    val title: String
+) {
+    fun toGame(): Game {
+        return Game(
+            developer,
+            freeToGameProfileUrl,
+            gameUrl,
+            genre,
+            id,
+            platform,
+            publisher,
+            releaseDate,
+            shortDescription,
+            thumbnail,
+            title
+        )
+    }
+}
