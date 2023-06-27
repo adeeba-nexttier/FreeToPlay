75b5402ba13b0f8d0f1b02ad98661343fbcbdb20
Adeeba Khan <adeeba.khan@nexttier.io>
2023-06-27 10:58:46 +0500

    state hoisting

--- app/src/main/java/com/dev/freetoplay/presentation/theme/activity/MainActivity.kt
+++ app/src/main/java/com/dev/freetoplay/presentation/theme/activity/MainActivity.kt
@@ -7,7 +7,10 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
+import androidx.compose.material.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.dev.freetoplay.presentation.theme.FreeToPlayTheme
@@ -34,6 +37,20 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun Greeting(name: String) {
     Text(text = "Hello $name!")
+}
+
+@Composable
+fun StateFulComposable() {
+    val text = remember { mutableStateOf(value = "") }
+    TextField(
+        value = text.value, 
+        onValueChange = {
+            text.value = it
+        },
+        label = {
+            Text(text = "TextField")
+        }
+    )
 }
 
 @Preview(showBackground = true)
